cb5ba7a0ba192c7c4a99e411980a4da9848558d7
Tom Payne <twpayne@gmail.com>
2020-08-22 21:30:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
-if [ "$CODESPACES" == "true" ] ; then
-	chmod 700 $HOME/dotfiles
-	curl -sfL https://git.io/chezmoi | sh
-	./bin/chezmoi init --apply --clone=false --source=$HOME/dotfiles
+set -e # -e: exit on error
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
 fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"

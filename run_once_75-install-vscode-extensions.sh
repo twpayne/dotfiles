be7c59769f31bf9696aebc00c190f8a695e27c03
Tom Payne <twpayne@gmail.com>
2020-09-05 17:48:41 +0200

    Install EditorConfig.EditorConfig VS Code extension

--- run_once_75-install-vscode-extensions.sh
+++ run_once_75-install-vscode-extensions.sh
@@ -6,12 +6,13 @@ which code > /dev/null || exit 0
 # Install desired extensions.
 code --force --install-extension alefragnani.project-manager
 code --force --install-extension eamodio.gitlens
+code --force --install-extension EditorConfig.EditorConfig
 code --force --install-extension golang.Go
 code --force --install-extension ms-python.python
 code --force --install-extension ms-vscode.cpptools
 code --force --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --force --install-extension redhat.vscode-yaml
 code --force --install-extension stkb.rewrap
-code --force --install-extension tyriar.sort-lines
+code --force --install-extension Tyriar.sort-lines
 code --force --install-extension yzhang.markdown-all-in-one
 code --force --install-extension zxh404.vscode-proto3

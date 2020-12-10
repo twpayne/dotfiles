748859ad806e30beda9121492976251e52f90d23
Tom Payne <twpayne@gmail.com>
2020-12-10 19:27:26 +0100

    Enable AWS profile in powerlevel10k

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -74,7 +74,7 @@
     # plenv                 # perl version from plenv (https://github.com/tokuhirom/plenv)
     kubecontext             # current kubernetes context (https://kubernetes.io/)
     # terraform             # terraform workspace (https://www.terraform.io)
-    # aws                   # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
+    aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
     # aws_eb_env            # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
     azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)

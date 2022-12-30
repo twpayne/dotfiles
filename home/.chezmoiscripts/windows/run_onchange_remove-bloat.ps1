366a1e322bfdb8f21c23ee848df9e5c2ffd339d8
Tom Payne <twpayne@gmail.com>
2022-12-29 17:55:32 -0800

    Add initial Windows bloat removal script

---
+++ home/.chezmoiscripts/windows/run_onchange_remove-bloat.ps1
@@ -0,0 +1,35 @@
+# FIXME this script needs to be run as Administrator
+
+Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
+Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
+Get-AppxPackage -AllUsers Microsoft.MSPaint | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Microsoft3DViewer | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.MixedReality.Portal | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.ScreenSketch | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.StorePurchaseApp | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Windows.Photos | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsAlarms | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsCalculator | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsCamera | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsCommunicationsApps | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsFeedbackHub | Remove-AppPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsMaps | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.WindowsStore | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxApp | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.ZuneVideo | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.People | Remove-AppxPackage
+Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage

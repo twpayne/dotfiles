8f5317d376d4a9048d534bc2b3c47ab7fbb78392
Tom Payne <tom.payne@flarm.com>
2023-01-12 15:52:22 +0100

    Remove some annoying oh-my-zsh aliases

---
+++ home/exact_dot_oh-my-zsh/exact_custom/exact_lib/misc.zsh
@@ -0,0 +1,25 @@
+autoload -Uz is-at-least
+
+# *-magic is known buggy in some versions; disable if so
+if [[ $DISABLE_MAGIC_FUNCTIONS != true ]]; then
+  for d in $fpath; do
+    if [[ -e "$d/url-quote-magic" ]]; then
+      if is-at-least 5.1; then
+        autoload -Uz bracketed-paste-magic
+        zle -N bracketed-paste bracketed-paste-magic
+      fi
+      autoload -Uz url-quote-magic
+      zle -N self-insert url-quote-magic
+    break
+    fi
+  done
+fi
+
+## jobs
+setopt long_list_jobs
+
+env_default 'PAGER' 'less'
+env_default 'LESS' '-R'
+
+# recognize comments
+setopt interactivecomments

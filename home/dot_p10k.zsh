66446ee77aad06e5a73c0013e0383d0e726453f9
Tom Payne <tom.payne@flarm.com>
2024-03-06 15:05:25 +0100

    Activate more powerlevel10k plugins

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -55,15 +55,15 @@
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     # direnv                # direnv status (https://direnv.net/)
-    # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     # anaconda              # conda environment (https://conda.io/)
-    # pyenv                 # python environment (https://github.com/pyenv/pyenv)
+    pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                # node.js version from nodenv (https://github.com/nodenv/nodenv)
     # nvm                   # node.js version from nvm (https://github.com/nvm-sh/nvm)
     # nodeenv               # node.js environment (https://github.com/ekalinin/nodeenv)
     # node_version          # node.js version
-    # go_version            # go version (https://golang.org)
+    go_version              # go version (https://golang.org)
     # rust_version          # rustc version (https://www.rust-lang.org)
     # dotnet_version        # .NET version (https://dotnet.microsoft.com)
     # rbenv                 # ruby version from rbenv (https://github.com/rbenv/rbenv)

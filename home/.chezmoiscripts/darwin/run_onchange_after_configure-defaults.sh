2c89b48d969d46287cf6575c658194f3a39f01e9
Tom Payne <twpayne@gmail.com>
2024-04-28 12:53:30 +0200

    Update macOS defaults

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -2,7 +2,23 @@
 
 set -eufo pipefail
 
-defaults write -g NSAutomaticCapitalizationEnabled -bool false
-defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write -g AppleEnableSwipeNavigateWithScrolls -int 0
+defaults write -g AppleMiniaturizeOnDoubleClick -int 0
+defaults write -g ApplePressAndHoldEnabled -int 0
+defaults write -g CGDisableCursorLocationMagnification -int 0
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+defaults write -g NSAutomaticCapitalizationEnabled -int 0
+defaults write -g NSAutomaticDashSubstitutionEnabled -int 0
+defaults write -g NSAutomaticInlinePredictionEnabled -int 0
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -int 0
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -int 0
+defaults write -g NSAutomaticSpellingCorrectionEnabled -int 0
+defaults write -g NSAutomaticTextCorrectionEnabled -int 0
 defaults write -g NSUserDictionaryReplacementItems '()'
-defaults write -g com.apple.trackpad.forceClick -bool false
+defaults write -g WebAutomaticSpellingCorrectionEnabled -int 0
+defaults write -g com.apple.keyboard.fnState -int 1
+defaults write -g com.apple.swipescrolldirection -int 0
+defaults write -g com.apple.trackpad.forceClick -int 0
+
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true

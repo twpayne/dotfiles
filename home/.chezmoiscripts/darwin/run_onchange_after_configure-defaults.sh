4d82e3ecc72b3b8d30d223dce86aeea06de786fe
Tom Payne <twpayne@gmail.com>
2024-10-12 14:42:49 +0200

    Tweak macOS defaults

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -5,6 +5,7 @@ set -eufo pipefail
 defaults write -g AppleEnableSwipeNavigateWithScrolls -int 0
 defaults write -g AppleMiniaturizeOnDoubleClick -int 0
 defaults write -g ApplePressAndHoldEnabled -int 0
+defaults write -g AppleShowAllExtensions -int 1
 defaults write -g CGDisableCursorLocationMagnification -int 0
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
@@ -15,10 +16,23 @@ defaults write -g NSAutomaticPeriodSubstitutionEnabled -int 0
 defaults write -g NSAutomaticQuoteSubstitutionEnabled -int 0
 defaults write -g NSAutomaticSpellingCorrectionEnabled -int 0
 defaults write -g NSAutomaticTextCorrectionEnabled -int 0
+defaults write -g NSDocumentSaveNewDocumentsToCloud -int 0
 defaults write -g NSUserDictionaryReplacementItems '()'
 defaults write -g WebAutomaticSpellingCorrectionEnabled -int 0
+
 defaults write -g com.apple.keyboard.fnState -int 1
 defaults write -g com.apple.swipescrolldirection -int 0
 defaults write -g com.apple.trackpad.forceClick -int 0
 
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock orientation -string left
+defaults write com.apple.dock show-recents -int 0
+defaults write com.apple.dock showDesktopGestureEnabled -int 0
+defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
+defaults write com.apple.dock showMissionControlGestureEnabled -int 0
+
+defaults write com.apple.finder _FXShowPosixPathInTitle -int 1
+defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+defaults write com.apple.finder _FXSortFoldersFirst -int 1
+defaults write com.apple.finder FXRemoveOldTrashItems -int 1
+defaults write com.apple.finder FXEnableExtensionChangeWarning -int 0

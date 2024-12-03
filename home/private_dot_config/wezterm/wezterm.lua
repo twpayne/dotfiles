6d20e3d02d1a6b47042ca30e78a22537cdaeaf4d
Tom Payne <twpayne@gmail.com>
2024-12-03 18:02:45 +0100

    Open new WezTerm windows in home directory

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -5,6 +5,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+config.keys = {
+  { key = 'n', mods = 'CMD', action = wezterm.action.SpawnCommandInNewWindow { cwd=wezterm.home_dir } },
+}
 config.window_decorations = 'RESIZE'
 
 return config

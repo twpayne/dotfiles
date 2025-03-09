d7e552071328ef81fcb9d56cfd5f73f0641dfdbc
Tom Payne <twpayne@gmail.com>
2025-03-09 19:19:56 +0100

    Remove deprecated brew --no-lock flag

--- home/.chezmoiscripts/darwin/run_onchange_before_install-google-earth-pro.sh
+++ home/.chezmoiscripts/darwin/run_onchange_before_install-google-earth-pro.sh
@@ -4,6 +4,6 @@ set -eufo pipefail
 
 sudo softwareupdate --agree-to-license --install-rosetta
 
-brew bundle --no-lock --file=/dev/stdin <<EOF
+brew bundle --file=/dev/stdin <<EOF
 cask "google-earth-pro"
 EOF

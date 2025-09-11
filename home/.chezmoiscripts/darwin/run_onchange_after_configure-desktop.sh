1145361f1f7f36a061bf99e4356d1d6e647a6ccb
Tom Payne <twpayne@gmail.com>
2025-09-11 12:02:04 +0200

    Add AeroSpace config

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
@@ -2,4 +2,6 @@
 
 set -eufo pipefail
 
+defaults write com.apple.spaces spans-displays -bool true && killall SystemUIServer
+
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Space Gray Pro.png" as POSIX file'
